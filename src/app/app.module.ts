6653a9c8f83294dffafa9aeab409ab392ddb71a2
Selvin Medina <selvinmedina0016@gmail.com>
2021-02-21 21:35:03 -0600

    work

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,8 +17,6 @@ import { StoreModule } from '@ngrx/store';
 
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './auth/login/login.component';
-import { RegisterComponent } from './auth/register/register.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { IngresoEgresoComponent } from './ingreso-egreso/ingreso-egreso.component';
 import { EstadisticaComponent } from './ingreso-egreso/estadistica/estadistica.component';
@@ -29,21 +27,16 @@ import { OrdenIngresoPipe } from './pipes/orden-ingreso.pipe';
 import { ChartsModule } from 'ng2-charts';
 import { AuthModule } from './auth/auth/auth.module';
 import { SharedModule } from './shared/shared.module';
+import { IngresoEgresoModule } from './ingreso-egreso/ingreso-egreso.module';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    DashboardComponent,
-    IngresoEgresoComponent,
-    EstadisticaComponent,
-    DetalleComponent,
-    OrdenIngresoPipe,
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
     ChartsModule,
     SharedModule,
     AuthModule,
+    IngresoEgresoModule,
     StoreModule.forRoot(appReducers),
     StoreDevtoolsModule.instrument({
       maxAge: 25, // Retains last 25 states
@@ -52,7 +45,6 @@ import { SharedModule } from './shared/shared.module';
     AngularFireModule.initializeApp(environment.firebase),
     AngularFirestoreModule,
     AngularFireAuthModule,
-    ReactiveFormsModule,
     AppRoutingModule,
   ],
   providers: [],
