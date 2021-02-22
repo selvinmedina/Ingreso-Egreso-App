930e354710d0f6d1692478dfa8776a6294181b8c
Selvin Medina <selvinmedina0016@gmail.com>
2021-02-21 21:35:10 -0600

    w

--- src/app/ingreso-egreso/ingreso-egreso.module.ts
+++ src/app/ingreso-egreso/ingreso-egreso.module.ts
@@ -1,12 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import { DashboardComponent } from '../dashboard/dashboard.component';
+import { IngresoEgresoComponent } from './ingreso-egreso.component';
+import { EstadisticaComponent } from './estadistica/estadistica.component';
+import { DetalleComponent } from './detalle/detalle.component';
+import { OrdenIngresoPipe } from '../pipes/orden-ingreso.pipe';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  declarations: [
+    DashboardComponent,
+    IngresoEgresoComponent,
+    EstadisticaComponent,
+    DetalleComponent,
+    OrdenIngresoPipe,
+  ],
+  imports: [CommonModule, ReactiveFormsModule],
 })
-export class IngresoEgresoModule { }
+export class IngresoEgresoModule {}
